c4dc13fd0a12be5914c17ede0cf4a282e0b1ac9e
Vinicius Porto <vinicius.alves.porto@gmail.com>
2019-05-09 12:52:06 -0300

    ex 1 lista 4

---
+++ lista4/notas.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int mostFrequent(int arr[], int n) 
+{ 
+   
+  
+    // find the max frequency using linear traversal 
+    int max_count = 1, res = arr[0], curr_count = 1; 
+    for (int i = 1; i < n; i++) { 
+        if (arr[i] == arr[i - 1]) 
+            curr_count++; 
+        else { 
+            if (curr_count > max_count) { 
+                max_count = curr_count; 
+                res = arr[i - 1]; 
+            } 
+            curr_count = 1; 
+        } 
+    } 
+  
+    //If last element is most frequent 
+    if (curr_count > max_count) 
+    { 
+        max_count = curr_count; 
+        res = arr[n - 1]; 
+    } 
+  
+    return res; 
+} 
+  
+// driver program 
+int main() 
+{ 
+    int cout ;
+    int arr[] = { 1, 5, 2, 1, 3, 2, 1, 2}; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    cout = mostFrequent(arr, 2); 
+    printf("%d \n", cout);
+    return 0; 
+} 
