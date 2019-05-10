6a1cb1a98d339299c3ca8d75e231413665a6c9cc
Marcos Vinicius Lima Raimundo <marcoslimaraimundo@gmail.com>
2019-05-10 20:15:09 -0300

    change notas.c

--- lista4/notas.c
+++ lista4/notas.c
@@ -31,8 +31,27 @@ int mostFrequent(int arr[], int n)
 // driver program 
 int main() 
 { 
-    int cout ;
-    int arr[] = { 1, 5, 2, 1, 3, 2, 1, 2}; 
+    int cout, tam, a ;
+    tam = 10;
+    int arr[] = {20, 25, 85, 40, 25, 90, 25, 40, 55, 40};
+
+    for (int i = 0; i < tam; ++i)
+    {
+        for (int j = i + 1; j < tam; ++j)
+        {
+            if (arr[i] < arr[j])
+            {
+                a = arr[i];
+                arr[i] = arr[j];
+                arr[j] = a;
+            }
+        }
+    }
+    for (int i = 0; i < tam; ++i)
+    {
+        printf("%d\n", arr[i]);
+    }
+
     int n = sizeof(arr) / sizeof(arr[0]); 
     cout = mostFrequent(arr, 2); 
     printf("%d \n", cout);
